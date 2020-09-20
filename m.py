4adea9c4c69f8d5a31f1e09595d1fc228915d9b5
meghnamanoj <53592787+meghnamanoj@users.noreply.github.com>
2020-09-20 14:06:04 +0530

    Update m.py

--- m.py
+++ m.py
@@ -10,3 +10,7 @@ b = int(b)
 while b!= 0:
     a , b = b ,a%b
 print(a)
+'''here we write a in the for a = qb +r
+    and the while loop the new a becomes b and new b becomes remainder of a/b(a%b)
+    the loop continues until the new b becomes zero ,the final a is the gcd(a,b)'''
+
