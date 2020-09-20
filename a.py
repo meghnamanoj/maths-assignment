ba2536dba60d9e7b09d4bc4c65c79e2a938db0d4
meghnamanoj <53592787+meghnamanoj@users.noreply.github.com>
2020-09-20 19:24:08 +0530

    Update a.py

--- a.py
+++ a.py
@@ -26,3 +26,10 @@ while a%b != 0:
     p,q = rnew - x*pnew,snew - x*qnew
 print(p,q)
 print(str(b),'=',str(p),'*',str(A),'+',str(q),'*',str(B))
+'''Bezouts identity
+    gcd(a,b)= ax+by
+    the algorithm finds  x and y.
+    Here 'a = bb and b = aa-x*bb 'these 2 functions finds the gcd and
+    'r,s = pnew,qnew and p,q = rnew - x*pnew,snew - x*qnew ' finds the coeficient of a and b.
+    the function 'rnew - x*pnew' finds the coefficient of a and 'snew - x*qnew' finds the coeficient
+    of b.''' 
